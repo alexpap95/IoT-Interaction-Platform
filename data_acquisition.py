72069bc13b819bda1320cd8316b6e368446f9e90
Alexandros Papadopoulos <43494202+alexpap95@users.noreply.github.com>
2018-10-30 00:01:57 +0200

    Update data_acquisition.py

--- data_acquisition.py
+++ data_acquisition.py
@@ -79,6 +79,7 @@ class myThread(threading.Thread):
         self.fuse=fuse
     def run(self):
         time.sleep(5)
+        for n in range(4500):
             time.sleep(0.1)
             self.fuse.write_to_file()
         print("done")
