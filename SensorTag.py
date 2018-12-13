de9a7be20a0625d0d677eced4081df92af0935bf
Alexandros Papadopoulos <43494202+alexpap95@users.noreply.github.com>
2018-12-13 15:36:38 +0200

    13.12

--- SensorTag.py
+++ SensorTag.py
@@ -57,7 +57,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
     client.on_disconnect = on_disconnect
-    client.connect("192.168.1.103", 1883, 60)
+    client.connect("192.168.1.132", 1883, 60)
     client.loop_forever(0.01)
 
 def s16(value):
